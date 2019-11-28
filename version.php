b742a86ae8865369df074895c239b32442f2679e
Paul Holden <paulh@moodle.com>
2019-11-28 18:46:43 +0000

    Version bump for new release.

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'report_upgradelog';
-$plugin->release   = '1.0';
-$plugin->version   = 2019112100;
+$plugin->release   = '1.1';
+$plugin->version   = 2019112800;
 $plugin->requires  = 2018051700; // Moodle 3.5 onwards.
 $plugin->maturity  = MATURITY_STABLE;
